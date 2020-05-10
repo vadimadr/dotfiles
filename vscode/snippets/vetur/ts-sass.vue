9a87a3a1990a291f0f8e4f758e07cf34be84fd56
Vadim Andronov <vadimadr@gmail.com>
2020-05-10 18:43:08 +0300

    Add vscode, ranger, git

---
+++ vscode/snippets/vetur/ts-sass.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>{{ text }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Foo extends Vue {
+  text = 'bar';
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
