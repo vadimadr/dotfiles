4d38832a0eaa980b121312c546ff7e4c2ace1e6d
Vadim Andronov <vadimadr@gmail.com>
2026-02-02 22:28:27 +0000

    Add kitty config

--- zsh/.zshrc.d/bindkeys.zsh
+++ zsh/.zshrc.d/bindkeys.zsh
@@ -15,6 +15,13 @@ bindkey '^[[3;5~' kill-word
 # Ctrl+backspace = C-w
 bindkey '^H' backward-kill-word
 
+# Kitty / Cursor-style: Opt+Backspace, Opt+Left/Right, Opt+Delete
+# (Kitty sends these sequences; Cmd+Backspace/Left/Right use Ctrl+U/A/E which are default)
+bindkey '\e^?'   backward-kill-word   # Opt+Backspace
+bindkey '\e[1;3D' backward-word       # Opt+Left
+bindkey '\e[1;3C' forward-word        # Opt+Right
+bindkey '\e[3;3~' kill-word           # Opt+Delete (forward)
+
 # use crtl + up / ctrl + down to iterate through dir stack (oh-my-zsh/dircycle)
 bindkey '^[[1;5A' insert-cycledleft
 bindkey '^[[1;5B' insert-cycledright
